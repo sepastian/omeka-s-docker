e752749b678d7d17ec54ac1dacf6adb2e5f1ff91
Sebastian Gassner <sebastian.gassner@uni-passau.de>
2024-06-24 17:37:01 +0200

    setting db password correctly
    adding themes in docker compose
    
    enabling logging during startup

--- files/php-fpm/entrypoint.sh
+++ files/php-fpm/entrypoint.sh
@@ -1,11 +1,17 @@
 #!/bin/sh
 
 mariadb_password=$(cat ${MARIADB_PASSWORD_FILE})
+cat <<EOF > /var/www/html/config/database.ini
+user="$MARIADB_USER"
+password="$mariadb_password"
+dbname="$MARIADB_DATABASE"
+host="$MARIADB_HOST"
+EOF
+
+# Enable logging by setting 'log' => true in /var/www/html/config/local.config.php.
+# See https://omeka.org/s/docs/user-manual/errorLogging/#log-errors
 sed -i \
-    -e "s|user.*|user=\"$MARIADB_USER\"|" \
-    -e "s|password.*|password=\"$mariadb_password\"|" \
-    -e "s|dbname.*|dbname=\"$MARIADB_DATABASE\"|" \
-    -e "s|host.*|host=\"$MARIADB_HOST\"|" \
-    /var/www/html/config/database.ini
+    -e "s|'log' => false,|'log' => true,|" \
+    /var/www/html/config/local.config.php
 
 php-fpm
